8b53b275e224423da25c5a5419c6b66790731353
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-15 09:55:57 +0000

    UPdated file

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 This script retrieves information about a user's TODO list progress
-from a REST API based on the provided employee ID and exports
-it to JSON format.
+from a REST API based on the provided employee ID.
 """
 
 import json
@@ -31,21 +30,38 @@ def get_employee_todo_progress(employee_id):
     todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
     todos_data = todos_response.json()
 
-    # Construct JSON data
+    # Count total and completed tasks
+    total_tasks = len(todos_data)
+    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+
+    # Display progress
+    print(f"Employee {employee_name} is done with tasks "
+          f"({completed_tasks}/{total_tasks}):")
+
+    # Display completed tasks
+    for todo in todos_data:
+        if todo['completed']:
+            print(f"\t{todo['title']}")
+
+    # Export data to JSON file
     json_data = {str(employee_id): [{"task": todo['title'], "completed":
                  todo['completed'], "username": employee_name}
                  for todo in todos_data]}
-
-    # Write data to JSON file
     filename = f"{employee_id}.json"
     with open(filename, 'w') as jsonfile:
         json.dump(json_data, jsonfile, indent=4)
     print(f"Task data exported to '{filename}' successfully.")
 
+    # Check if correct user ID
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        if employee_id != int(list(data.keys())[0]):
+            print("Warning: Correct USER_ID not found in the JSON file.")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 2-export_to_JSON.py <employee_id>")
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
     employee_id = sys.argv[1]
