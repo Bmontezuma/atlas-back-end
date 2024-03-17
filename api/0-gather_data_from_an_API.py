ed9446e700f7152256fb043fefd9a070b635baae
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-03-17 12:33:23 +0000

    Updated Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import os
+"""Script to fetch and display employee TODO list progress."""
+
 import sys
 import requests
 
 
 def get_employee_data(employee_id):
     """
-    This function fetches employee data from an API.
+    Fetch employee data from an API.
+
     :param employee_id: The ID of the employee to fetch data for.
     :return: A dictionary containing employee data.
     """
@@ -15,21 +17,35 @@ def get_employee_data(employee_id):
     if response.status_code == 200:
         return response.json()
     else:
-        raise Exception(f"Error: Unable to fetch data
-                        for employee ID {employee_id}.")
+        raise Exception(
+            f"Error: Unable to fetch data for employee ID {employee_id}.")
 
 
 def main():
     """
     The main function of the script.
     """
-    employee_id = 1
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    try:
+        employee_id = int(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
     employee_data = get_employee_data(employee_id)
+    total_tasks = len(employee_data['todos'])
+    completed_tasks = sum(
+        1 for task in employee_data['todos'] if task['completed'])
+
     print(f"Employee {employee_data['name']} is done with tasks
-          ({employee_data['completed_tasks']}
-          / {employee_data['total_tasks']}): ")
-    for task in employee_data['tasks']:
-        print(f"\t{task['title']}")
+          ({completed_tasks}/{total_tasks}): ")
+    for task in employee_data['todos']:
+        if task['completed']:
+            print(f"\t{task['title']}")
 
 
 if __name__ == "__main__":
